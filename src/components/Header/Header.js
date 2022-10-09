e7bcca0669138f6d6cc3402cfa2319b189807106
mdmahfuz0709 <mdmahfuz0709@gmail.com>
2022-10-09 21:14:20 +0600

    Header File Complete

---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Header.css'
+import logo from '../../images/Logo.svg';
+
+const Header = () => {
+    return (
+        <header className='header-area'>
+            <nav className='nav-area'>
+                <div className="header-logo">
+                    <img src={logo} alt="ema-john" />
+                </div>
+                <div className="header-menu">
+                    <ul>
+                        <li><a href="#.">Order</a></li>
+                        <li><a href="#.">Order</a></li>
+                        <li><a href="#.">Manage Inventory</a></li>
+                        <li><a href="#.">Login</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
