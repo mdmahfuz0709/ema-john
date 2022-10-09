99f8b6bc843aa9472f77a8af8aadb36c4e8e6db3
mdmahfuz0709 <mdmahfuz0709@gmail.com>
2022-10-10 01:26:52 +0600

    Create Shop File And Complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
                 <div className="header-menu">
                     <ul>
                         <li><a href="#.">Order</a></li>
-                        <li><a href="#.">Order</a></li>
+                        <li><a href="#.">Order Review</a></li>
                         <li><a href="#.">Manage Inventory</a></li>
                         <li><a href="#.">Login</a></li>
                     </ul>
---
+++ src/components/Product/Product.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Product.css';
+
+const Product = (props) => {
+    const { name, img, seller, price, ratings } = props.product;
+
+    return (
+        <div className='card'>
+            <div className="card-img">
+                <img src={img} alt="" />
+            </div>
+            <div className="card-body">
+                <h4>{name}</h4>
+                <p>Price: ${price}</p>
+                <p><small>Manufacturer: {seller}</small></p>
+                <p><small>Rating: {ratings} star</small></p>
+            </div>
+
+            <button>Add to Card</button>
+        </div>
+    );
+};
+
+export default Product;
